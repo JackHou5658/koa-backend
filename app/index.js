7b9e02e00f99fbbc23787cc4211bfaa01372ba49
BigD <109411790+BigD5658@users.noreply.github.com>
2024-02-19 02:37:28 +0800

    修正監聽端口

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const bodyParser = require("koa-bodyparser");
 const error = require("koa-json-error");
 const parameter = require("koa-parameter");
 const mongoose = require("mongoose");
-const cors = require('koa2-cors');//後端做cros
+const cors = require("koa2-cors"); //後端做cros
 const app = new Koa();
 const routing = require("./routes");
 const { param } = require("./routes/home");
@@ -23,4 +23,6 @@ app.use(cors());
 app.use(bodyParser());
 app.use(parameter(app)); //加入一個app就可以在ctx加入function去驗證
 routing(app);
-app.listen(5501, () => console.log("port:5501"));
+const port = process.env.PORT || 8080; // 使用環境變數PORT或預設端口
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
